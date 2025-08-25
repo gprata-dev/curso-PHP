c476d9ffeb7966a185ba4a993ab953d4d6285b3a
Guilherme Prata Peres <121191877+gprata-dev@users.noreply.github.com>
2025-08-24 22:50:34 -0300

    primeiras aulas e revisão de array associativo visto no estágio

---
+++ aulas/2 - revisão arrayAssoc.php
@@ -0,0 +1,7 @@
+<?php
+    $arr_assoc = ['nome' => 'Guilherme', 'idade' => 21];
+
+    print_r($arr_assoc);
+    echo "<br>";
+    echo $arr_assoc['nome'];
+?>
---
+++ aulas/3 - variáveis.php
@@ -0,0 +1,17 @@
+<?php
+    $a = "nome";
+
+    echo "$a <br>";
+
+    $$a = "Guilherme";
+
+    echo "$nome <br>";
+
+    echo 'a';
+    echo "<br>";
+    echo $a;
+    echo "<br>";
+    echo '$a';
+    echo "<br>";
+    echo "$a";
+?>
