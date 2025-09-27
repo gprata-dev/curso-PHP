7ed3740de13b74198ce8154d023f1d3fd9aa6a11
Guilherme Prata Peres <121191877+gprata-dev@users.noreply.github.com>
2025-09-27 19:15:57 -0300

    adicionado foreach na aula 13

--- aulas/13_foreach.php
+++ aulas/13_foreach.php
@@ -4,3 +4,13 @@
     foreach($nomes as $nome) {
         echo "O nome atual é $nome <br>";
     }
+
+    echo "<br>";
+
+    $produtos = ['porta' => 100, 'maçaneta' => 5, 'motor' => 2000, 'retrovisor' => 8];
+
+    foreach ($produtos as $produto => $preco) {
+        if($preco > 10) {
+            echo "O produto $produto custa mais do 10 <br>";
+        }
+    }
