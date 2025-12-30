4284248b64e78da96b9a1da99bc95c0c312410a6
Guilherme Prata Peres <121191877+gprata-dev@users.noreply.github.com>
2025-12-30 16:42:26 -0300

    continuação da aula de strings

--- aulas/19_strings.php
+++ aulas/19_strings.php
@@ -35,4 +35,65 @@
     echo "$s.";
     //ltrim();
     //rtrim();
+
+    echo "<br><br>";
+
+    echo strtolower($s) . "<br>";
+
+    echo strtoupper($s) . "<br>";
+
+    $frase = "testando php <br>";
+
+    echo ucfirst($frase);
+    echo ucwords($frase);
+
+    echo "<br>";
+
+    $textoHtml = "<p>Testando parágrafo.</p><div>Uma div</div><p>Outro parágrafo</p>";
+
+    echo $textoHtml . "<br>";
+
+    echo strip_tags($textoHtml);
+
+    echo "<br><br>";
+
+    $str = "PHP não eh legal";
+
+    echo substr($str, 0, 3);
+    echo substr($str, 8) . "<br>";
+    echo substr($str, -5) . "<br>";
+    echo substr($str, 0, -13) . "<br>";
+
+    echo "<br><br>";
+
+    echo strrev("Testando") . "<br>";
+    echo str_repeat("Testando 2 ", 5);
+
+    echo "<br><br>";
+
+    print_r(explode(" ", "Junkrat: Vai explodir!"));
+    echo "<br>";
+    print_r(explode(",", "Carro, Barco, Avião, Balsa"));
+
+    echo "<br><br>";
+
+    $str1 = "Teste do método strpos, preciso repetir a palavra strpos";
+    echo strpos($str1, "strpos") . "<br>";
+    if (strpos($str1, "pos") === false) {
+        echo "Não encontrado 1" . "<br>";
+    } else if (strpos($str, "pas") === false) {
+        echo "Não encontrado 2" . "<br>";
+    }
+
+    echo strrpos($str1, "strpos") . "<br>"; //também retorna falso caso não ache
+
+    //extrair palavra sem precisar do indice
+    echo substr($str1, strpos($str1, "repetir"), 7) . "<br>";
+
+    echo strstr($str1, "preciso"); //também retorna falso caso não ache
+
+    echo "<br><br>";
+
+    $url = "https://www.stackoverflow.com/bar?search=php";
+    print_r(parse_url($url));
 ?>
