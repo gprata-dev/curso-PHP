8c86dd836ce2ab85c66754f12b0e4242dca915cd
Guilherme Prata Peres <121191877+gprata-dev@users.noreply.github.com>
2026-01-06 23:19:38 -0300

    nova aula, última parte de arrays

---
+++ aulas/22_arrays_3.php
@@ -0,0 +1,30 @@
+<?php 
+    $arr = [1,2,3,4,5];
+
+    $arrRev = array_reverse($arr);
+    print_r($arr);
+    echo "<br>";
+    print_r($arrRev);
+    echo "<br><br>";
+
+    shuffle($arr);
+    print_r($arr);
+    echo "<br>";
+
+    echo array_sum($arr) . "<br><br>";  //ignora o que não for numérico no array
+
+    $arr2 = [6,7,8,9,'10'];
+
+    $arrMerge = array_merge($arr, $arr2, ['a', true]); //aceita quantos argumentos for necessário
+    print_r($arrMerge);
+    echo "<br><br>";
+
+    $arr3 = [1,2,3];
+    $arr4 = [3,4,5];
+
+    print_r(array_diff($arr3, $arr4));
+    echo "<br>";
+    print_r(array_diff($arr4, $arr3));
+    echo "<br>";
+    print_r(array_diff($arr4, $arr3, [4]));
+?>
