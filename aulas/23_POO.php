0ffbdb5659b3c3fc7d94664bd68ace1e300a57c3
Guilherme Prata Peres <121191877+gprata-dev@users.noreply.github.com>
2026-01-18 23:20:35 -0300

    nova aula incrementada no mesmo arquivo

--- aulas/23_POO.php
+++ aulas/23_POO.php
@@ -1,11 +1,13 @@
 <?php 
-    class Car {
+    class Car
+    {
         public int $rodas  = 4;
         public int $aro;
         public string $cor = "Vermelha"; //não é necessário int, string... mas recomendado
 
-        function ligar() {
-            echo "Motor ligado.";
+        function ligar()
+        {
+            echo "Motor ligado. <br>";
         }
     }
 
@@ -24,4 +26,48 @@
     $ferrari->ligar();
 
     //echo $ferrari->ligar();
+
+
+
+    class Animal
+    {
+        private string $name;   //atributos sempre privados
+        private const PATAS = 4;
+
+        function setName($nome) //ao não colocar modificador de visibilidade, o padrão é público / não recomendado
+        {
+            $this->name = $nome;
+        }
+
+        public function latir()
+        {
+            return "Au au";
+        }
+        
+        public function latirForte()
+        {
+            return strtoupper($this->latir());
+        }
+
+        public function getName()
+        {
+            return $this->name;
+        }
+
+        public function getPaws()
+        {
+            return self::PATAS;
+        }
+    }
+
+    $nanda = new Animal;
+
+    $nanda->setName("Nanda");
+    //echo "O nome é $nanda->nome" //não recomendado: encapsulamento
+    //echo "O nome é $nanda->getName()"; errado: strings
+    echo "O nome é {$nanda->getName()} <br>";
+    //ou
+    echo "O nome é " . $nanda->getName() . "e ela tem " . $nanda->getPaws() . " patas<br>"; //se fosse public const: $nanda::PATAS
+
+    
 ?>
