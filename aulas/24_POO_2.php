1305b519b550f4ea6aa6fbe0caeb22d24508f3fa
Guilherme Prata Peres <121191877+gprata-dev@users.noreply.github.com>
2026-01-25 20:56:26 -0300

    nova aula

--- aulas/24_POO_2.php
+++ aulas/24_POO_2.php
@@ -34,5 +34,60 @@
     // $matheus->alterarRodas($carro); fatal error: método de uma classe tentando acessar atributo privado de outra
     $matheus->alterarRodas($onibus);
     echo $carro->getRodas() . "<br>";
-    echo $onibus->getRodas();
+    echo $onibus->getRodas() . "<br>";
+
+    
+    class Veiculo
+    {
+        protected function mover(int $metros)
+        {
+            echo "Se moveu $metros metros <br>";
+        }
+    }
+
+    class Moto extends Veiculo
+    {
+        public function acessarMover(int $metros)
+        {
+            $this->mover($metros);
+        }
+    }
+
+    $honda = new Moto;
+    // $honda->mover(30);     erro
+    $honda->acessarMover(30); //se mover() fosse private: erro
+
+    if($honda instanceof Moto) {
+        echo "Honda é uma moto. <br>";
+    }
+    if ($honda instanceof Veiculo) {
+        echo "Honda é um veículo. <br>";
+    }
+
+
+    interface Caracteristicas
+    {
+        const NOME = "Matheus";
+
+        public function falar(); //método abstrato
+    }
+
+    class Humano implements Caracteristicas
+    {
+        public $idade = 29;
+
+        public function falar()
+        {
+            echo "Olá <br>";
+        }
+
+        public function dizerNome()
+        {
+            echo "Meu nome é " . self::NOME . ".<br>";
+        }
+    }
+
+    $matheus = new Humano;
+    $matheus->falar();
+    $matheus->dizerNome();
 ?>
