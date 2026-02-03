0eaa21e6672a0f0ac24fe22697f9281bac39a9bb
Guilherme Prata Peres <121191877+gprata-dev@users.noreply.github.com>
2026-02-02 21:52:37 -0300

    nova aula

--- aulas/25_POO_3.php
+++ aulas/25_POO_3.php
@@ -26,5 +26,77 @@
     $x = new Central;
     $x->teste();
     $x->traitTeste();
-    echo $x->y; //se fosse protected dá erro!
+    echo $x->y . "<br>"; //se fosse protected dá erro!
+
+    
+    class Carro
+    {
+        private string $cor;
+        public string $marca;
+
+        function __construct($cor, $marca)
+        {
+            $this->setColor($cor);
+            $this->setMarca($marca);
+        }
+        public function setColor($cor)
+        {
+            $this->cor = $cor;
+        }
+        public function setMarca($marca)
+        {
+            $this->marca = $marca;
+        }
+        public function getCor()
+        {
+            return $this->cor;
+        }
+        public function getMarca()
+        {
+            return $this->marca;
+        }
+    }
+
+    $camaro = new Carro("Amarelo", "Chevrolet");
+    echo $camaro->getCor() . "<br>";
+
+
+    $pessoa = new class() {
+        private string $nome = "Guilherme";
+
+        public function dizerNome()
+        {
+            return "Olá meu nome é {$this->nome} <br>";
+        }
+    };
+
+    echo $pessoa->dizerNome();
+
+
+    if (class_exists("Carro")) {
+        echo "Carro existe <br>";
+    }
+
+    print_r(get_class_vars("Carro")); //função chamada no global: apenas public aparece
+    echo "<br>";
+    print_r(get_class_methods("Carro"));
+    echo "<br>";
+
+    $x = 2;
+    if (is_object($pessoa)) {
+        echo "É objeto <br>";
+
+        var_dump(get_class($pessoa));
+        echo "<br>";
+    }
+    var_dump(get_class($camaro));
+    echo "<br>";
+
+    if (!is_object($x)) {
+        echo "Não é objeto <br>";
+    }
+
+    if (method_exists($pessoa, "dizerNome")) {
+        echo "Método existe";
+    }
 ?>
