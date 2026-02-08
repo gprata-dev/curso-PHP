4687d28b6c4290f9b81b1676cde1aace05947b6e
Guilherme Prata Peres <121191877+gprata-dev@users.noreply.github.com>
2026-02-07 23:45:52 -0300

    nova aula

--- aulas/26_datas.php
+++ aulas/26_datas.php
@@ -4,9 +4,16 @@
     echo date('Y-m-d H:i:s');
     echo "<br>";
     echo date("D, M - Y");
-    echo "<br>";
+    echo "<br><br>";
+
 
     $cincoDias = strtotime("5 days");
     echo $cincoDias . "<br>";
-    echo date('d/m/y', $cincoDias);
+    echo date('d/m/y', $cincoDias) . "<br><br>";
+
+
+    $dataNascimento = mktime(02, 12, 33, 02, 05, 1991); //hora, minuto, segundo, mês, dia, ano
+    echo $dataNascimento . "<br>";
+
+    echo date('d/m/Y', $dataNascimento) . "<br><br>";
 ?>
