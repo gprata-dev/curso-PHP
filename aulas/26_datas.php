b8644fac71344c15801117bc5ab4e859a9537f41
Guilherme Prata Peres <121191877+gprata-dev@users.noreply.github.com>
2026-02-17 23:15:12 -0300

    novas aulas

--- aulas/26_datas.php
+++ aulas/26_datas.php
@@ -16,4 +16,38 @@
     echo $dataNascimento . "<br>";
 
     echo date('d/m/Y', $dataNascimento) . "<br><br>";
+
+
+    $dataAtual = new DateTime();
+    print_r($dataAtual);
+    echo "<br>";
+    echo $dataAtual->format('l - F - y') . "<br>";
+
+    $dataAtual->modify('-1 day');
+    echo $dataAtual->format('l - F - y') . "<br>";
+
+    $dataAtual->setDate(1999, 12, 01);
+    print_r($dataAtual);
+    echo "<br>";
+    $dataAtual->setTime(5, 12, 10);
+    print_r($dataAtual);
+    echo "<br>";
+    echo $dataAtual->format('l - F - y') . "<br><br>";
+
+    $diferenca = $dataAtual->diff(new DateTime());
+    print_r($diferenca);
+    echo "<br>";
+    echo $diferenca->format('%a days') . "<br>";
+
+    $realAtual = new DateTime();
+    if ($realAtual > $dataAtual) {
+        echo "A hora atual de verdade ({$realAtual->format('d - m - y h:i')}) é maior do que ({$dataAtual->format('d - m - y')})<br><br>";
+    } else {
+        echo "Menor <br><br>";
+    }
+    
+    date_default_timezone_set('America/Sao_Paulo');
+    // print_r($realAtual); não muda -> objeto criado antes
+    $realMesmoAtual = new DateTime();
+    print_r($realMesmoAtual);
 ?>
